c1aa0b923827f60d6852a83a937666fa13468b52
mqksrwq <maksim4ikb2006@gmail.com>
2026-03-29 22:32:13 +0300

    add driver and vehicle models

---
+++ OOP_Cource/Models/Driver.cs
@@ -0,0 +1,10 @@
+namespace OOP_Cource.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string LicenseNumber { get; set; }
+        public string AssignedVehicle { get; set; }
+    }
+}
---
+++ OOP_Cource/Models/Vehicle.cs
@@ -0,0 +1,11 @@
+namespace OOP_Cource.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public string Model { get; set; }
+        public int Capacity { get; set; }
+        public string Status { get; set; }
+    }
+}
