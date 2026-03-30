f3ade568118957a6856c960822b557229417a84a
mqksrwq <maksim4ikb2006@gmail.com>
2026-03-30 19:17:38 +0300

    add task to welcome form

--- OOP_Cource/Form1.Designer.cs
+++ OOP_Cource/Form1.Designer.cs
@@ -30,42 +30,55 @@
         {
             this.lblWelcome = new System.Windows.Forms.Label();
             this.btnNext = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblWelcome
             // 
             this.lblWelcome.AutoSize = true;
             this.lblWelcome.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblWelcome.Location = new System.Drawing.Point(41, 46);
+            this.lblWelcome.Location = new System.Drawing.Point(16, 44);
             this.lblWelcome.Name = "lblWelcome";
-            this.lblWelcome.Size = new System.Drawing.Size(900, 47);
+            this.lblWelcome.Size = new System.Drawing.Size(949, 47);
             this.lblWelcome.TabIndex = 0;
-            this.lblWelcome.Text = "Добро пожаловать в систему учета автобуного парка!";
+            this.lblWelcome.Text = "Курсовая работа - Бояркин Максим - 24ВП1 - Вариант 15";
+            this.lblWelcome.UseCompatibleTextRendering = true;
             // 
             // btnNext
             // 
             this.btnNext.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnNext.Location = new System.Drawing.Point(46, 105);
+            this.btnNext.Location = new System.Drawing.Point(438, 283);
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(135, 44);
+            this.btnNext.Size = new System.Drawing.Size(151, 61);
             this.btnNext.TabIndex = 1;
             this.btnNext.Text = "Далее";
             this.btnNext.UseVisualStyleBackColor = true;
             this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.label1.Location = new System.Drawing.Point(17, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(994, 148);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Задание: «Автобусный парк»: учет транспортных средств в автопарке,\r\nмаршрутов, во" +
+    "дителей, учета доходов и расходов (оплата за проезд, ремонт и\r\nт. п.), выдача от" +
+    "четов по запросам.\r\n\r\n";
+            this.label1.UseCompatibleTextRendering = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1064, 241);
+            this.ClientSize = new System.Drawing.Size(1066, 356);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnNext);
             this.Controls.Add(this.lblWelcome);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Автобусный парк";
+            this.Text = "Курсовая работа - Бояркин - 24ВП1";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,6 +88,7 @@
 
         private System.Windows.Forms.Label lblWelcome;
         private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label label1;
     }
 }
 
